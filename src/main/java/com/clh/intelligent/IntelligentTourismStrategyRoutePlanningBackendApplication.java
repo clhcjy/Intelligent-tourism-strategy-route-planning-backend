42debc0f1f702340886891139432fc3c0595782d
clhcjy <1754598483@qq.com>
2024-10-30 16:22:28 +0800

    1030陈烈烘

---
+++ src/main/java/com/clh/intelligent/IntelligentTourismStrategyRoutePlanningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.clh.intelligent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentTourismStrategyRoutePlanningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelligentTourismStrategyRoutePlanningBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clh/intelligent/IntelligentTourismStrategyRoutePlanningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clh.intelligent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentTourismStrategyRoutePlanningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
